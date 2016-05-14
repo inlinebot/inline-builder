90e425d3821b8d5a7b330537e62099083292f2d3
Panjamapong Sermsawatsri <panjmp@gmail.com>
2016-05-14 16:24:33 +0700

    initial commit add to dockerhub

---
+++ index.js
@@ -0,0 +1,6 @@
+
+require('./push-to-dockerhub')('jitta', 'spinal');
+
+/*
+DOCKERHUB_EMAIL=panjmp@gmail.com DOCKERHUB_USERNAME=panj DOCKERHUB_PASSWORD=Abcd6712 node index.js
+*/
---
+++ push-to-dockerhub.js
@@ -0,0 +1,51 @@
+const ejs = require('ejs');
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+
+const templateString = `{
+  "builders":[{
+    "type": "docker",
+    "image": "node:5",
+    "export_path": "image.tar",
+    "login": false
+  }],
+  "provisioners":[
+    {
+      "type": "shell",
+      "inline": [
+        "git clone https://github.com/<%= username %>/<%= repository %>.git",
+        "cd <%= repository %>",
+        "npm install"
+      ]
+    }
+  ],
+  "post-processors": [
+    [
+      {
+        "type": "docker-import",
+        "repository": "inlinebot/module-<%= repository %>",
+        "tag": "latest"
+      },
+      {
+        "type": "docker-push",
+        "login": true,
+        "login_email": "<%= dockerHub.email %>",
+        "login_username": "<%= dockerHub.username %>",
+        "login_password": "<%= dockerHub.password %>"
+      }
+    ]
+  ]
+}`;
+
+const template = ejs.compile(templateString);
+
+module.exports = (username, repository) => {
+  const dockerHub = {
+    email: process.env.DOCKERHUB_EMAIL,
+    username: process.env.DOCKERHUB_USERNAME,
+    password: process.env.DOCKERHUB_PASSWORD
+  };
+  fs.writeFileSync(`module-${repository}.json`, template({ username, repository, dockerHub }));
+  execSync(`packer build module-${repository}.json`, { stdio:[0,1,2] });
+  fs.unlinkSync(`${repository}.json`);
+};
